b60d68f90aef62d21ff8578f05024525f0a8cc53
Nam Le <thenamle2005@gmail.com>
2026-01-01 15:07:43 -0800

    fix: vercel file reduction attempt

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import app from '../backend/src/server';
-import connectDB from '../backend/src/utils/db';
-
-export default async (req: any, res: any) => {
-  await connectDB();
-  return app(req, res);
-};
-
